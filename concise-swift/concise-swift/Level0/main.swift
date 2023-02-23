7f067a7aebc4cf402f703e9b26d19eec2a657e69
haha1haka <haha1haka@icloud.com>
2023-02-24 00:25:56 +0900

    [27] 편지

--- concise-swift/concise-swift/Level0/main.swift
+++ concise-swift/concise-swift/Level0/main.swift
@@ -8,7 +8,11 @@
 import Foundation
 
 // MARK: - 26. 배열 자르기
-func solution(_ numbers:[Int], _ num1:Int, _ num2:Int) -> [Int] {
-    return (num1...num2).map { numbers[$0] }
-}
+//func solution(_ numbers:[Int], _ num1:Int, _ num2:Int) -> [Int] {
+//    return (num1...num2).map { numbers[$0] }
+//}
 
+// MARK: - 27. 편지
+func solution(_ message:String) -> Int {
+    return message.count * 2
+}
